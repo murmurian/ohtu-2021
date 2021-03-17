a31918926c85ce0b553f100b03d4e0a147e3f93f
János Pap <murmurian@gmail.com>
2021-03-17 16:23:09 +0200

    viikko3 tehtävä 11

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -30,6 +30,25 @@ public class Stepdefs {
         element.click();
     }
 
+    @Given("user with username {string} with password {string} is successfully created")
+    public void userIsSuccessfullyCreated(String username, String password) {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        createAccount(username, password, password);
+        pageHasContent("Welcome to Ohtu Application!");
+    }
+
+    @Given("user with username {string} and password {string} is tried to be created")
+    public void userCreationFails(String username, String password) {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+        createAccount(username, password, password);
+        pageHasContent("username should have at least 3 characters");
+        pageHasContent("password should have at least 8 characters");
+    }
+
     @When("correct username {string} and password {string} are given")
     public void correctUsernameAndPasswordAreGiven(String username, String password) {
         logInWith(username, password);
